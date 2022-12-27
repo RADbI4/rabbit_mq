fef42896cd670cbd5c0a84b0934be81424d52ea4
Radmir <capitanpit@yandex.ru>
2022-12-27 19:15:01 +0300

    add from new branch

--- first_rabbit_mq_script/receiving.py
+++ first_rabbit_mq_script/receiving.py
@@ -3,6 +3,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
